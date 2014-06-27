b6fdedf3e4da5ce59d1957a20dcc5d6bd32cb1cb
Pankaj Bhamare <pbhamare@gmail.com>
2014-06-27 17:04:06 +0100

    shows whether user is in ot Out of the defined area

--- app/src/main/java/com/crowdsight/mobile/app/TrackLocation.java
+++ app/src/main/java/com/crowdsight/mobile/app/TrackLocation.java
@@ -16,44 +16,83 @@ import android.widget.TextView;
 public class TrackLocation extends ActionBarActivity {
 
     Button tl;
-    TextView textLat, textLong;
+    TextView textLat, textLong, FtextLong, FtextLat, dValue, rText;
+
+    //change the radius to define the limited area
+    double radius = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_location);
 
-        tl = (Button) findViewById(R.id.button_tl);
         textLat = (TextView) findViewById(R.id.textLat);
         textLong = (TextView) findViewById(R.id.textLong);
+
+        FtextLong = (TextView) findViewById(R.id.FtextLong);
+        FtextLat = (TextView) findViewById(R.id.FtextLat);
+        dValue = (TextView) findViewById(R.id.dValue);
+        rText = (TextView) findViewById(R.id.rText);
+
+
         LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         LocationListener ll = new myLocationListener();
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
+        //change NETWORK_PROVIDER to GPS_PROVIDER to compile and run on Emulator
+        // NETWORK_PROVIDER does not support Emulator. 
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, ll);
 
-//        tl.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//
-//
-//
-//            }
-//        });
 
 
     }
 
-    private class myLocationListener implements LocationListener{
+    private class myLocationListener implements LocationListener   {
+
+
+
         @Override
-        public void onLocationChanged(Location location) {
-            if (location != null) {
-                double pLong = location.getLongitude();
-                double pLat = location.getLatitude();
+        public void onLocationChanged(Location userLocation) {
+            if (userLocation != null) {
+                double pLong = userLocation.getLongitude();
+                double pLat = userLocation.getLatitude();
 
                 textLat.setText(Double.toString(pLat));
                 textLong.setText(Double.toString(pLong));
+
+                // fixed location values for stadium
+
+                Location fixedLocation = new Location("fLocation");
+                // change this value as per your co-ordinates
+                // you can get the co-ordinates when you run it 1st time on your device
+                // record the coordinates and change accordingly then change the radius above
+                fixedLocation.setLatitude(53.3533363);
+                fixedLocation.setLongitude(-6.2290848);
+                double fLat = fixedLocation.getLatitude();
+                double fLong = fixedLocation.getLongitude();
+
+                FtextLat.setText(Double.toString(fLat));
+                FtextLong.setText(Double.toString(fLong));
+
+                //calculating the distance
+
+                float Distance = userLocation.distanceTo(fixedLocation);
+
+                dValue.setText(Double.toString(Distance));
+
+                if (Distance > radius){
+
+                    rText.setText("Sorry You are out !");
+
+                } else
+
+
+                rText.setText("You are in !!");
+
+
+
             }
         }
+
+
 
         @Override
         public void onStatusChanged(String s, int i, Bundle bundle) {
@@ -74,9 +113,11 @@ public class TrackLocation extends ActionBarActivity {
 
 
 
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.track_location, menu);
         return true;
